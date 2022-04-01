609ec07fa18cb5134cf83734b5aff64b1669560a
RS4POWER <regulasalexandru@yahoo.com>
2022-04-01 20:57:15 +0300

    first

---
+++ src/main/java/com/example/mgmtspring12/MgmtSpring12Application.java
@@ -0,0 +1,13 @@
+package com.example.mgmtspring12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgmtSpring12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MgmtSpring12Application.class, args);
+    }
+
+}
